9bf612db65046603ed542c11f07ec98c64c8c8e5
rajrishav368 <rajrishav368@gmail.com>
2023-02-27 18:17:54 +0530

    twelve

--- src/hirecandidates/Employer.jsx
+++ src/hirecandidates/Employer.jsx
@@ -7,10 +7,12 @@ import body from "../assets/employee.png";
 const Employer = () => {
   return (
     <div className="hire__employerdirection_emp">
+    <div className="hire__signup_emp">
       <div className="hire__signup_logo_emp">
         <Link to="/">
           <img src={logo} alt="" />
         </Link>
+      </div>
       </div>
 
       <div className="hire__header_text_emp">
@@ -22,7 +24,6 @@ const Employer = () => {
         </div>
       </div>
 
-       <div className="hire__straight_emp">
         <div className="hire__employers_image_emp">
           <div className="hire__employers_image1_emp">
             <img src={body} alt=""></img>
@@ -37,9 +38,9 @@ const Employer = () => {
                 <h3>Create an account</h3>
                 <p>
                   This is a very quick process that only requires your personal
-                  <br></br>
+                  
                   information, as well as a brief description of the company
-                  <br></br>
+    
                   you're working for
                 </p>
               </div>
@@ -52,13 +53,13 @@ const Employer = () => {
                 <h3>Publish Vacancies</h3>
                 <p>
                   Once you have an account, you can create a vacancy by going
-                  <br></br>
+                
                   to the "Vacancy" section on the top, and creating a new offer.
-                  <br></br>
+                  
                   in order to make the vacancy available for the applicants, you
-                  <br></br>
+                 
                   need to fill out general information about your vacancy and
-                  <br></br>
+                 
                   then proceed to upload it.
                 </p>
               </div>
@@ -71,20 +72,19 @@ const Employer = () => {
                 <h3>Hire candidates</h3>
                 <p>
                   Once the vacancy is published, other companies will be able to
-                  <br></br>
+             
                   view your offer, and provide their reserve employees for the
-                  <br></br>
+                 
                   position. You can also search for candidates from the search
-                  <br></br>
+                  
                   bar. You will then get the chance to review the applications
                   and
-                  <br></br>
+                
                   find the best candidate for you!
                 </p>
               </div>
             </div>
           </div>
-        </div>
       </div>
       <div className="hire__continue_button_emp">
         <Link to="/signupEmployer">
