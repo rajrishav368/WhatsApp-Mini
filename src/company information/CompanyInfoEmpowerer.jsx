4c87d1d9cd9b076e97723db9b048f422ce1d349c
rajrishav368 <rajrishav368@gmail.com>
2023-02-28 14:05:10 +0530

    first

--- src/company information/CompanyInfoEmpowerer.jsx
+++ src/company information/CompanyInfoEmpowerer.jsx
@@ -9,30 +9,29 @@ import linkedin from "../assets/linkedin.png";
 import facebook from "../assets/facebook.png";
 import telegram from "../assets/telegram.png";
 import youtube from "../assets/youtube.png";
-import {useState} from "react";
+import { useState } from "react";
 import Select from "react-select";
 import { useMemo } from "react";
 import countryList from "react-select-country-list";
-import downarrow from "../assets/downarrow.png"
+import downarrow from "../assets/downarrow.png";
 import { Link } from "react-router-dom";
 
-const CompanyEmployer = () => {
+const CompanyEmpowerer = () => {
+  const [value, setValue] = useState("");
+  const options = useMemo(() => countryList().getData(), []);
 
-  const [value,setValue]= useState("");
-  const options= useMemo(()=>countryList().getData(),[]);
-
-  const changeHandler=(value)=>{
+  const changeHandler = (value) => {
     setValue(value);
-    
   };
-  
+
   return (
     <div className="hire__employerempowerer">
       <div className="hire__header_ciepw">
         <Link to="/">
-        <div className="hire__header_logo_ciepw">
-          <img src={logo} alt=""></img>
-        </div></Link>
+          <div className="hire__header_logo_ciepw">
+            <img src={logo} alt=""></img>
+          </div>
+        </Link>
         <div className="hire__header_username_ciepw">
           <div className="hire__header_username_background_ciepw">
             <h5>U</h5>
@@ -46,16 +45,16 @@ const CompanyEmployer = () => {
         </div>
       </div>
       <div className="hire__companyinformation_ciepw">
-        <h2></h2>
+        <h2>Company information</h2>
       </div>
 
       <div className="hire__upload_photo_ciepw">
         <div className="hire__camera_pic_ciepw">
           <img src={camera} alt=""></img>
         </div>
-      </div>
-      <div className="hire__upload_text_ciepw">
-        <h3>upload company logo</h3>
+        <div className="hire__upload_text_ciepw">
+          <h3>upload company logo</h3>
+        </div>
       </div>
 
       <div className="hire__employee_data_ciepw">
@@ -63,13 +62,13 @@ const CompanyEmployer = () => {
           <input type="text" placeholder="Company name"></input>
         </div>
         <div className="hire__company_mail_ciepw">
-          <input className="email" placeholder="Contact E-mail" ></input>
+          <input className="email" placeholder="Contact E-mail"></input>
         </div>
         <div className="hire__phone_number_ciepw">
           <input type="text" placeholder="Phone number optional"></input>
         </div>
         <div className="hire__country_dropdown_ciepw">
-          <Select options={options} value={value} onChange={changeHandler}/>
+          <Select options={options} value={value} onChange={changeHandler} />
         </div>
         <div className="hire__state_region_ciepw">
           <input type="text" placeholder="Region/state"></input>
@@ -80,43 +79,39 @@ const CompanyEmployer = () => {
         <div className="hire__company_description_ciepw">
           <input type="text" placeholder="Company description"></input>
         </div>
+      </div>
+      <div className="hire__company_links_ciepw">
+        <h2>Add your links:</h2>
+      </div>
+      <div className="hire__social_links_ciepw">
+        <div className="hire__www_ciepw">
+          <img src={www} alt=""></img>
+        </div>
+        <div className="hire__linkedin_ciepw">
+          <img src={linkedin} alt=""></img>
+        </div>
+        <div className="hire__facebook_ciepw">
+          <img src={facebook} alt=""></img>
+        </div>
+        <div className="hire__github_ciepw">
+          <img src={github} alt=""></img>
+        </div>
+        <div className="hire__instagram_ciepw">
+          <img src={instagram} alt=""></img>
+        </div>
+        <div className="hire__telegram_ciepw">
+          <img src={telegram} alt=""></img>
+        </div>
+        <div className="hire__youtube_ciepw">
+          <img src={youtube} alt=""></img>
+        </div>
+      </div>
 
-        <div className="hire__company_links_ciepw">
-          <h2>Add your links:</h2>
-        </div>
-        <div className="hire__social_links_ciepw">
-          <div className="hire__www_ciepw">
-            <img src={www} alt=''></img>
-          </div>
-          <div className="hire__linkedin_ciepw">
-            <img src={linkedin} alt=""></img>
-          </div>
-          <div className="hire__facebook_ciepw">
-            <img src={facebook} alt=""></img>
-          </div>
-          <div className="hire__github_ciepw">
-            <img src={github} alt=""></img>
-          </div>
-          <div className="hire__instagram_ciepw">
-            <img src={instagram} alt=""></img>
-          </div>
-          <div className="hire__telegram_ciepw">
-            <img src={telegram} alt=""></img>
-          </div>
-          <div className="hire__youtube_ciepw">
-            <img src={youtube} alt=""></img>
-          </div>
-        </div>
-
-        <div className="hire__create_button_ciepw">
-          <button type="button">Create an account</button>
-        </div>
-        
-      
+      <div className="hire__create_button_ciepw">
+        <button type="button">Create an account</button>
       </div>
     </div>
   );
-}
+};
 
-
-export default CompanyEmployer;
+export default CompanyEmpowerer;
--- src/company information/CompanyInfoWorkforce.jsx
+++ src/company information/CompanyInfoWorkforce.jsx
@@ -9,31 +9,29 @@ import linkedin from "../assets/linkedin.png";
 import facebook from "../assets/facebook.png";
 import telegram from "../assets/telegram.png";
 import youtube from "../assets/youtube.png";
-import {useState} from "react";
+import { useState } from "react";
 import Select from "react-select";
 import { useMemo } from "react";
 import countryList from "react-select-country-list";
-import downarrow from "../assets/downarrow.png"
+import downarrow from "../assets/downarrow.png";
 import { Link } from "react-router-dom";
 
-const CompanyEmployer = () => {
+const CompanyWorkfoce = () => {
+  const [value, setValue] = useState("");
+  const options = useMemo(() => countryList().getData(), []);
 
-  const [value,setValue]= useState("");
-  const options= useMemo(()=>countryList().getData(),[]);
-
-  const changeHandler=(value)=>{
+  const changeHandler = (value) => {
     setValue(value);
-    
   };
   return (
     <div className="hire__employerworkforce">
       <div className="hire__header_ciwf">
-      <Link to="/">
-        <div className="hire__header_logo_ciwf">
-          <img src={logo} alt=""></img>
+        <Link to="/">
+          <div className="hire__header_logo_ciwf">
+            <img src={logo} alt=""></img>
           </div>
-      </Link>
-      <div className="hire__header_username_ciwf">
+        </Link>
+        <div className="hire__header_username_ciwf">
           <div className="hire__header_username_background_ciwf">
             <h5>U</h5>
           </div>
@@ -46,16 +44,16 @@ const CompanyEmployer = () => {
         </div>
       </div>
       <div className="hire__companyinformation_ciwf">
-        <h2></h2>
+        <h2>Company information</h2>
       </div>
 
       <div className="hire__upload_photo_ciwf">
         <div className="hire__camera_pic_ciwf">
           <img src={camera} alt=""></img>
         </div>
-      </div>
-      <div className="hire__upload_text_ciwf">
-        <h3>upload company logo</h3>
+        <div className="hire__upload_text_ciwf">
+          <h3>upload company logo</h3>
+        </div>
       </div>
 
       <div className="hire__employee_data_ciwf">
@@ -63,13 +61,13 @@ const CompanyEmployer = () => {
           <input type="text" placeholder="Company name"></input>
         </div>
         <div className="hire__company_mail_ciwf">
-          <input className="email" placeholder="Contact E-mail" ></input>
+          <input className="email" placeholder="Contact E-mail"></input>
         </div>
         <div className="hire__phone_number_ciwf">
           <input type="text" placeholder="Phone number optional"></input>
         </div>
         <div className="hire__country_dropdown_ciwf">
-          <Select options={options} value={value} onChange={changeHandler}/>
+          <Select options={options} value={value} onChange={changeHandler} />
         </div>
         <div className="hire__state_region_ciwf">
           <input type="text" placeholder="Region/state"></input>
@@ -80,43 +78,40 @@ const CompanyEmployer = () => {
         <div className="hire__company_description_ciwf">
           <input type="text" placeholder="Company description"></input>
         </div>
+      </div>
 
-        <div className="hire__company_links_ciwf">
-          <h2>Add your links:</h2>
+      <div className="hire__company_links_ciwf">
+        <h2>Add your links:</h2>
+      </div>
+      <div className="hire__social_links_ciwf">
+        <div className="hire__www_ciwf">
+          <img src={www} alt=""></img>
         </div>
-        <div className="hire__social_links_ciwf">
-          <div className="hire__www_ciwf">
-            <img src={www} alt=''></img>
-          </div>
-          <div className="hire__linkedin_ciwf">
-            <img src={linkedin} alt=""></img>
-          </div>
-          <div className="hire__facebook_ciwf">
-            <img src={facebook} alt=""></img>
-          </div>
-          <div className="hire__github_ciwf">
-            <img src={github} alt=""></img>
-          </div>
-          <div className="hire__instagram_ciwf">
-            <img src={instagram} alt=""></img>
-          </div>
-          <div className="hire__telegram_ciwf">
-            <img src={telegram} alt=""></img>
-          </div>
-          <div className="hire__youtube_ciwf">
-            <img src={youtube} alt=""></img>
-          </div>
+        <div className="hire__linkedin_ciwf">
+          <img src={linkedin} alt=""></img>
         </div>
+        <div className="hire__facebook_ciwf">
+          <img src={facebook} alt=""></img>
+        </div>
+        <div className="hire__github_ciwf">
+          <img src={github} alt=""></img>
+        </div>
+        <div className="hire__instagram_ciwf">
+          <img src={instagram} alt=""></img>
+        </div>
+        <div className="hire__telegram_ciwf">
+          <img src={telegram} alt=""></img>
+        </div>
+        <div className="hire__youtube_ciwf">
+          <img src={youtube} alt=""></img>
+        </div>
+      </div>
 
-        <div className="hire__create_button_ciwf">
-          <button type="button">Create an account</button>
-        </div>
-        
-      
+      <div className="hire__create_button_ciwf">
+        <button type="button">Create an account</button>
       </div>
     </div>
   );
-}
+};
 
-
-export default CompanyEmployer;
+export default CompanyWorkfoce;
